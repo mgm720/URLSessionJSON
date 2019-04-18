255e0ca3c7ed2a556645861ea409eea9d1d23117
Michael Miles <michael.gage.miles@gmail.com>
2019-04-17 22:25:12 -0700

    Added URLSession GET and POST requests.

--- URLSessionJSON/ViewController.swift
+++ URLSessionJSON/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func getButtonTapped(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, poop) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(poop ?? "error")
+                }
+            }
+        }.resume()
+    }
 
-
+    @IBAction func postButtonTapped(_ sender: Any) {
+        
+        let params = ["username" : "@milesoftweets", "tweet" : "Yooooooo"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: params, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+        
+    }
+    
 }
 
